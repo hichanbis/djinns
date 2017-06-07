5dd5ed34faa96d3a3820c9b7295edc7fae9edf8d
hhocine <hhocine@jahia.com>
2017-06-07 12:49:52 +0200

    mouse pointer géré

---
+++ Assets/_Scripts/Battle/UI/HandlePointer.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class HandlePointer : MonoBehaviour, IPointerEnterHandler
+{
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        Button button = gameObject.GetComponent<Button>();
+        if (button.interactable)
+            button.Select();
+    }
+}
